50ccd39280581f74ce39a6f06c4a004256f9d610
Olfarmer <mail@clemens.jann.xyz>
2020-08-14 13:45:26 +0200

    Added calc

--- numbers.asm
+++ numbers.asm
@@ -1,5 +1,5 @@
 section	.data
-	message db "Hello, world", 10
+	message db "Starting...", 10
 
 section .text
 
@@ -8,17 +8,36 @@ _start:
 	mov	rax,1
 	mov	rdi,1
 	mov	rsi, message
-	mov	rdx, 14
+	mov	rdx, 12
 	syscall
+
+	;call	_calc
+	mov	rsi, 1	
+
+
+	;mov	rsi,rax
+	mov	rdi,1
+	mov	rax,1
+	mov	rdx,1
+	syscall
+
 	mov	rax, 60
 	mov	rdi, 0
 	syscall
 
-	mov	rax, 39
-	mov	rdi, 0
-	syscall
+_calc:			;Calculating numbers
+	mov	r8,16
+	mov	r9,1
+	mov	r10,1
 	
+_loop:	mov	rax,r9
+	mul	r10
+	
+	add	r9,1
+	add	r10,1
 
-	mov	rax, 60
-	mov	rdi, 0
-	syscall
+	cmp	rax,r8
+	jne	_loop
+
+	mov	rax,r8
+	ret
